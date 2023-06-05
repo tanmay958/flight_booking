3b1aaa6fa42a83e52dd0b60dc92c0d6f318d060f
tanmay958 <studytime958@gmail.com>
2023-06-06 00:08:29 +0530

    intial setup and error handling

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+const {PORT} = require('./config/serverConfig');
+
+const setupAndStartServer = () => {
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: true}));
+
+ 
+
+    app.listen(PORT, () => {
+        console.log(`Server started on port ${PORT}`);
+        // if(process.env.DB_SYNC) {
+        //     db.sequelize.sync({alter: true});
+        // }
+    });
+}
+
+setupAndStartServer();
